1e024cea4581f5ed33756e174d1cfb263ca5176f
noah <noa2006@naver.com>
2025-07-09 23:55:43 +0900

    서비스 테스트 작성, 도메인 테스트 작성, 통합테스트 작성
    (cherry picked from commit 069f42daa8c09f7f91d6b3020b05666e21c8ff8f)

--- src/main/java/io/hhplus/tdd/point/PointPolicy.java
+++ src/main/java/io/hhplus/tdd/point/PointPolicy.java
@@ -3,7 +3,7 @@ package io.hhplus.tdd.point;
 public class PointPolicy {
     public static final long MAX_BALANCE = 1_000_000L;// 최대잔고
     public static final long MIN_CHARGE_POINT = 100L;// 최소충전
-    public static final long MAX_CHARGE_POINT = 100_000L;// 최소충전
+    public static final long MAX_CHARGE_POINT = 100_000L;// 최대충전
     public static final long MIN_USE_POINT = 100L;//최소사용
 
     public static final String ERROR_MIN_CHARGE = "최소충전금액 미만";
